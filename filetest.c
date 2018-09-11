08ee6dac0c39c7a7c5580a30cdaf549d33145a3b
jshih23 <yoshih2100@gmail.com>
2018-09-10 23:50:18 -0400

    test files touched

--- filetest.c
+++ filetest.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-	printf("woo\n");
+	char ch, source_file[20], target_file[20];
+	FILE *source, *target;
+
+	printf("Enter name of file to copy from\n");
+	scanf("%s", source_file);
+
+	source = fopen(source_file, "r");
+
+
+	if( source == NULL )
+	{
+		printf("Press any key to exit...\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Enter name of target file\n");
+	scanf("%s", target_file);
+
+	target = fopen(target_file, "w");
+
+	if (target == NULL){
+		fclose(source);
+		printf("Press any key to exit...\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while ( (ch = fgetc(source)) != EOF){
+		fputc(ch, target);
+	}
+
+	printf("File copied successfully\n");
+
+	fclose(source);
+	fclose(target);
 	return 0;
 }
